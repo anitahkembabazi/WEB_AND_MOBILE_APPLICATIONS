2a6e9657c2378fcf335e8f9ba052e4a6bd2f8b38
anitahkembabazi <kembabaziritah0@gmail.com>
2024-09-12 17:43:15 +0300

    Create first_js_program.js

---
+++ WEEK_TWO/first_js_program.js
@@ -0,0 +1,33 @@
+// variables
+
+// start from python
+//age = int(70)
+
+//innitialisation
+//int age = 71;
+var my_name = "Anitah";
+
+
+// Declare a variable
+var height = 4.3;
+
+//Assign a variable 
+height = 3.4;
+
+// for printing output 
+console.log(my_name);
+
+console.log(height);
+
+//let
+let f_name;
+f_name = "Anitah"
+console.log(f_name)
+
+const pie = 3.14
+
+console.log("Name: ", my_name)
+
+
+
+
